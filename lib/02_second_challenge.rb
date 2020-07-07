2476d8540d0d33b2ddcd51fd528fa3e2c5737936
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 17:06:48 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch = {}
-      :matriarch = {},
-      :hero = {},
-      :hero_friends =[]
+      :patriarch => {}
+      :matriarch => {},
+      :hero => {},
+      :hero_friends =>[]
       
       #your key/value pairs here!
       }, 
     :capulet => {
-      :patriarch = {}
-      :matriarch = {},
-      :hero = {},
-      :heroine_friends = []
+      :patriarch => {}
+      :matriarch => {},
+      :hero => {},
+      :heroine_friends => []
       #your key/value pairs here!
       }
     
