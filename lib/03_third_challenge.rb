945ec5a8b6227eb8b72c84ac4440ff0403bbc78b
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 17:15:46 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -36,17 +36,7 @@ def third_challenge
         status: "alive"
         #your key/value pairs here
         },
-      :heroine_friends => [
-        {
-          name: "Steven",
-          age: "30",
-          attitude: "confused"
-        },
-        {
-         name:"Nurse",
-         age: "44",
-         attitude: "worried"}
-        ]
+      :heroine_friends => []
     }
   }
 
